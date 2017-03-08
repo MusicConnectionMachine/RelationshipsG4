f7bc626a4cc92202511f060908abe24d9f40d29d
Simon Zachau <zachau@in.tum.de>
2017-03-08 21:29:17 +0100

    working dummy with scraped urls

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,40 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const Graph = require('graphology');
-const hits = require('graphology-hits');
-
-
-const randomGraph = generateDummyGraph(10000);
-
-const {hubs, authorities} = hits(randomGraph);
-
-console.log("I'm done xD");
-
-
-
-function generateDummyGraph(numberOfNodes) {
-    const graph = new Graph();
-    // nodes
-    for(var i = 0; i < numberOfNodes; i++) {
-        graph.addNode(i);
-    }
-    // edges
-    for(var i = 0; i < numberOfNodes / 4; i++) {
-        const random1 = Math.floor(Math.random() * numberOfNodes);
-        const random2 = Math.floor(Math.random() * numberOfNodes);
-
-        const key = random1 + '->' + random2;
-        // add only if not already found:
-        if(!(graph.edges(random1, random2).includes(key))) {
-            graph.addEdgeWithKey(key, random1, random2);
-        }
-    }
-    return graph;
-}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.send(authorities);
+router.get('/', function (req, res, next) {
+    res.send('hello MusicConnectionMachine');
 });
 
 module.exports = router;
---
+++ server/source_reputability/calcuation.js
@@ -0,0 +1,96 @@
+const request = require('request');
+const Graph = require('graphology');
+const hits = require('graphology-hits');
+const fs = require('fs');
+
+const dummyURLs = [
+    'https://en.wikipedia.org/wiki/Johann_Sebastian_Bach',
+    'https://en.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart',
+    'https://en.wikipedia.org/wiki/Joseph_Haydn',
+    'https://en.wikipedia.org/wiki/Ludwig_van_Beethoven',
+    'https://en.wikipedia.org/wiki/Vienna'
+];
+
+generateGraph(dummyURLs).then(function (graph) {
+    scores = hits(graph).authorities;
+
+    const output = JSON.stringify(scores, undefined, 4);
+
+    // save
+    fs.writeFile('source_reputability/scores.json', output, function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('scores saved!');
+        }
+    });
+}).catch(function (error) {
+    console.log(error);
+});
+
+
+function generateGraph(urls) {
+    const graph = new Graph();
+    return new Promise(function (resolve, reject) {
+        Promise.all(
+            urls.map(function (url) {
+                return new Promise(function (resolve, reject) {
+                    if (!graph.hasNode(url)) {
+                        graph.addNode(url);
+                    }
+                    const domain = getDomain(url);
+                    if (domain) {
+                        getHTML(url).then(function (html) {
+                            getLinks(html, domain).forEach(function (link) {
+                                if (!graph.hasNode(link)) {
+                                    graph.addNode(link);
+                                }
+                                if (!graph.hasEdge(url, link)) {
+                                    const key = url + '->' + link;
+                                    graph.addEdgeWithKey(key, url, link);
+                                }
+                            });
+                            resolve();
+                        }).catch(function (error) {
+                            reject(error);
+                        });
+                    } else {
+                        reject('could not extract domain for ' + url);
+                    }
+                });
+            })
+        ).then(function () {
+            resolve(graph);
+        }).catch(function (error) {
+            reject(error);
+        });
+    });
+}
+
+function getDomain(url) {
+    const result = url.match(/http(s)?:\/\/(www\.)?.*?.(?=(\/|\?|#|$))/);
+    return result ? result[0] : null;
+}
+
+function getHTML(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                return resolve(body);
+            } else {
+                return reject(error);
+            }
+        });
+    });
+}
+
+function getLinks(html, domain) {
+    return html.match(/<a.*?>/g).map(function (link) {
+        // chop off beginning junk
+        link = link.replace(/.*href=('|")/, '');
+        // chop off ending junk
+        link = link.replace(/('|").*/g, '');
+        // add domain if necessary
+        return /http.*/.test(link) ? link : domain + link;
+    });
+}
