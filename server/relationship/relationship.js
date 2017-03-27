4418bc291ed9113f2e0475dc0c008d5fea30d225
Bauer Sandro <ga65bam@mytum.de>
2017-03-27 17:03:34 +0200

    added mozart text, uses this text for calling the nlp and filters it for openie

--- server/relationship/relationship.js
+++ server/relationship/relationship.js
@@ -6,7 +6,7 @@ const StanfordCoreNLPClient = require('../relationship/StanfordCoreNLPClient');
 
 const filter = require("../relationship/relationship.filter.js");
 
-const useLocalJson = true;
+const useLocalJson = false;
 
 const client = new StanfordCoreNLPClient(
     undefined,
@@ -28,12 +28,15 @@ if (useLocalJson) {
             filter.filterOpenIE(data);
         });
 } else {
-    getFileContent('test-data.wet')
+    getFileContent('mozart.txt')
         .catch(function (error) {
             console.log('error: reading file');
         }).then(function (data) {
             console.log("got data from wet");
-            var contentFirst = splitWet(data);
+            // for wet
+            // var contentFirst = splitWet(data);
+            // for txt
+            var contentFirst = data;
             console.log("Content: " + contentFirst);
             return client.annotate(contentFirst);
         }).catch(function (error) {
@@ -45,7 +48,8 @@ if (useLocalJson) {
             // TODO: save everything in DB
             // DB connection missing at the moment
 
-            filter.filterOpenIE(result);
+            var openie = filter.filterOpenIE(result);
+            console.log(JSON.stringify(openie));
             // TODO: filter data
             //filter.filterOpenIE(result);
             //filter.filterCorefs(result);
@@ -80,7 +84,6 @@ function splitWet(data) {
     // TODO: delete this later, just try to prevent timeout
     content = content.substring(0, 100);
 
-    content = "Chaconne, Italian Ciaccona, solo instrumental piece that forms the fifth and final movement of the Partita No. 2 in D Minor, BWV 1004, by Johann Sebastian Bach. Written for solo violin, the Chaconne is one of the longest and most challenging entirely solo pieces ever composed for that instrument.     Bach’s string compositions, including a half dozen partitas and sonatas for solo violin, were composed in the late 1710s and early 1720s, while Bach was employed at the court in Köthen, Germany. It was a period of great freedom and creativity for the composer.         The Chaconne forms the longest movement of the piece by far, making up roughly half of the entire partita. It draws upon the Baroque dance form known as a chaconne, in which a basic theme stated at the opening is then restated in several variations. In Bach’s Chaconne, the basic theme is four measures long, short and simple enough to allow for 64 variations. From a stern and commanding mood at the beginning, Bach gradually increases the complexity of his theme, mixing in various compositional effects. Some twists upon the theme are spacious and grand; others flow nimbly. Fast runs and large interval skips are frequent, requiring much dexterity from the performer. Bach also calls forth changes in emotional intensity, as some variations are dominated by long notes and others by many, more urgent short notes. Bach builds up his work over 256 measures, finally restating the theme at the end with new, even stronger harmonies.";
     return content;
 }
 
